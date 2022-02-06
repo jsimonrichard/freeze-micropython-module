301e203607cb04bdf53ced60192bd4849334105c
jsimonrichard <jsimonrichard@gmail.com>
2022-02-05 21:06:21 -0500

    Removed another syntax issue

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@
 cp -r $INPUT_PATH ~/micropython/ports/$INPUT_PORT/modules/$INPUT_MODULE
 make -C ~/micropython/ports/$INPUT_PORT
 
-local buildDir=("~/micropython/ports/$INPUT_PORT/build-*")
+buildDir=("~/micropython/ports/$INPUT_PORT/build-*")
 cp $buildDir/firmware.uf2 $INPUT_FILENAME
