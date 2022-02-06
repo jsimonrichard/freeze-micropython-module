1c3c2ef838f772ad1b207c0bdbcce4bfd9ec9cfe
jsimonrichard <jsimonrichard@gmail.com>
2022-02-05 20:48:27 -0500

    Add caching for apt package

--- setup-env.sh
+++ setup-env.sh
@@ -6,9 +6,27 @@ if [ $INPUT_PORT != "rp2" ]; then
     exit 1
 fi
 
-apt install gcc-arm-none-eabi -y
+if [ $CACHE_HIT == 'true' ]; then
+    # Restore the gcc-arm-none-eabi package
+    sudo cp --verbose --force --recursive ~/gcc-arm-none-eabi/* /
 
-git clone git@github.com:micropython/micropython.git ~/micropython
+else;
+    # Install arm compiler and prepare cache
+    sudo apt install gcc-arm-none-eabi -y
 
-make -C ~/micropython/mpy-cross
-make -C ~/micropython/ports/$INPUT_PORT submodules
+    mkdir -p ~/gcc-arm-none-eabi
+    sudo dpkg -L gcc-arm-none-eabi \
+        | while IFS= read -r f; do
+            if test -f $f; then
+                echo $f;
+            fi; 
+        done \
+        | xargs cp --parents --target-directory ~/gcc-arm-none-eabi/
+
+
+    # Clone and setup micropython
+    git clone https://github.com/micropython/micropython.git ~/micropython
+
+    make -C ~/micropython/mpy-cross
+    make -C ~/micropython/ports/$INPUT_PORT submodules
+fi
