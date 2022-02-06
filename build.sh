40be75bd2885577e104da242c4cfd337ed583630
jsimonrichard <jsimonrichard@gmail.com>
2022-02-05 21:01:52 -0500

    Fix error

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@
 cp -r $INPUT_PATH ~/micropython/ports/$INPUT_PORT/modules/$INPUT_MODULE
 make -C ~/micropython/ports/$INPUT_PORT
 
-local buildDir=("~/micropython/ports/$INPUT_PORT/build-*/firmware.uf2")
+local buildDir=("~/micropython/ports/$INPUT_PORT/build-*")
 cp $buildDir/firmware.uf2 $INPUT_FILENAME
